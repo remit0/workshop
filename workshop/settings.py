1c8b288d86b1c20d24be6aa19c75fcd84352b6ed
remi-rosenthal <remi.rosenthal@eleves.ec-nantes.fr>
2019-12-08 19:11:42 +0100

    first baseline submission notebook

--- workshop/settings.py
+++ workshop/settings.py
@@ -4,3 +4,4 @@ import os
 SETTINGS_PATH = os.path.abspath(__file__)
 ROOT_PATH = os.path.dirname(os.path.dirname(SETTINGS_PATH))     # two levels above
 DATA_PATH = os.path.join(ROOT_PATH, 'data')
+RESULTS_PATH = os.path.join(ROOT_PATH, 'results')
