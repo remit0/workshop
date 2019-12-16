eefd53952de7ae629b679b6df3837169a9c04b01
Rémi Rosenthal <remi-rosenthal@hotmail.fr>
2019-12-16 16:36:47 +0100

    updated compute efficiency - strategy one solution

--- workshop/classes.py
+++ workshop/classes.py
@@ -67,11 +67,12 @@ class BookMaker:
     MIN = 125   # min number of people visiting for a day
     MAX = 300   # max number of people visiting for a day
 
-    def __init__(self, groups: List[Group]):
+    def __init__(self, groups: List[Group], verbose: int =1):
 
         self.groups = groups    # on going groups (not assigned)
         self.assigned_groups = list()   # assigned groups
         self.bookings = pd.Series(0, index=range(1, 101))   # number of people per day
+        self.verbose = verbose
 
     def check_bounds(self) -> bool:
         """ checks that the min/max constraints are satisfied """
@@ -104,6 +105,9 @@ class BookMaker:
         self.assigned_groups.append(group)
         self.remove(group)
         self.bookings.loc[day] += group.size
+        if self.verbose:
+            print("**assigned group number {} to day {}.".format(group.num, group.assigned_day))
+            print("current bookings {}".format(self.bookings.sum()))
 
     def batch_assign(self, groups: List[Group], day: int):
         for group in groups:
@@ -112,35 +116,48 @@ class BookMaker:
     def evaluate_one_group(self, group: Group, day: int) -> float:
         """ greedily evaluates the cost of assigning the `group` to `day`. Does not actually assign
         the `group`. """
+        # todo : is there a way to compute over a smaller vector of bookings ?
         bookings_updated = self.bookings.copy()
         bookings_updated.loc[day] += group.size
-        new_cost = self.accounting_cost(bookings_updated) + group.evaluate(day)
-        return new_cost - self.cost()
+        return self.accounting_cost(bookings_updated) + group.evaluate(day)
 
     def evaluate_groups(self, day: int) -> List[Tuple[Group, float]]:
         """ returns a list of tuple (group, cost) sorted according to increasing cost """
-        # todo : is it actually useful to compute both acc penalty and group penalty ?
         groups_scored = list()
         for group in self.groups:
             groups_scored.append((group, self.evaluate_one_group(group, day)))
         groups_scored = sorted(groups_scored, key=lambda x: x[1], reverse=False)
         return groups_scored
 
-    def find_matching_groups(self, day: int, n: int) -> List[Group]:
-        """ finds the groups that minimizes the cost function to fill `n` people on `day` """
-        best_matches, total_size = list(), 0
-        groups_scored = self.evaluate_groups(day)
-        for group, _ in groups_scored:
-            total_size += group.size
-            best_matches.append(group)
-            if total_size >= n:
-                return best_matches
+    # ces deux fonctions c'est un peu overkill mais ça permet de réduire le temps de calcul
+    def get_best_wish(self, group: Group) -> int:
+        """ scores the wishes of a group and returns the less costly given current availabilities,
+        if no wish is open, returns -1.
+        Evaluates a group for a day 10 times. """
+        available_days = [day for day in group.days
+                          if self.bookings.loc[day] - group.size <= self.MAX]
+        return self.get_best_day(group, available_days)
+
+    def get_forced_wish(self, group: Group) -> int:
+        """ find the best matching day to assign a `group` that is not in its wishes :'( """
+        available_days = self.bookings[self.bookings <= self.MAX - group.size].index.tolist()
+        available_days = [day for day in available_days if day not in group.days]
+        return self.get_best_day(group, available_days)
+
+    def get_best_day(self, group: Group, days: List[int]) -> int:
+        chosen_day, min_cost = -1, float("inf") # pas super élégant mais permet de ne pas appeler sorted sinon ça prend 3 heures
+        for day in days:
+            day_score = self.evaluate_one_group(group, day)
+            if day_score < min_cost:
+                min_cost = day_score
+                chosen_day = day
+        return chosen_day
 
     def get_demand(self, rank: int, ascending: bool) -> List[int]:
         """ returns the list of days in ascending order for a given
         `rank` choice. (i.e. if we consider rank to be 0, computes the amount of people that have
         ranked each day with rank 0) """
-        # todo : shouldn't be demand conditional on actually available days ?
+        # todo : ça sert à rien pour le moment
         demand = pd.Series(0, index=range(1, 101))
         for group in self.groups:
             demand.loc[group.days[rank]] += group.size
